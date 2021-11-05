3cf0c2c3640069a937174cc0dab8bf88a37e3db6
Maxwell Mapako <wax911@users.noreply.github.com>
2021-11-05 11:45:01 +0200

    Update RequestInterceptor.java
    Adds explicit nullability annotations

--- src/main/java/com/example/demo/data/api/RequestInterceptor.java
+++ src/main/java/com/example/demo/data/api/RequestInterceptor.java
@@ -3,6 +3,7 @@ package com.example.demo.data.api;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 
@@ -10,8 +11,8 @@ import java.io.IOException;
  * Request interceptor for adding headers to our requests
  */
 public final class RequestInterceptor implements Interceptor {
-    @Override
-    public Response intercept(Chain chain) throws IOException {
+    @Override @NonNull
+    public Response intercept(@NonNull Chain chain) throws IOException {
         Request.Builder requestBuilder = chain.request().newBuilder();
         requestBuilder.addHeader("accept", "application/json");
         Request request = requestBuilder.build();
